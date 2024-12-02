a213f65eafdd4c320bc803f24ec46464cb060c47
angeldleon9 <130633456+angeldleon9@users.noreply.github.com>
2024-12-02 04:40:41 +0000

    UserButton for Sidebar

--- src/paginas/GeneradorRecibo.jsx
+++ src/paginas/GeneradorRecibo.jsx
@@ -39,7 +39,7 @@ const GeneradorRecibo = ()=> {
                 <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label>
                 <ul className="menu bg-base-200 min-h-full w-80 p-4">
                 {/* Sidebar content here */}
-                <li><a>Sidebar Item 1</a></li>
+                <li><a><UserButton /></a></li>
                 <li><a>Sidebar Item 2</a></li>
                 </ul>
             </div>
